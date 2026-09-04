e46f1383a5b571be509ba83e5e4c7dfcdfad6242
toanvk9 <toanvk9@ghtk.co>
2026-09-04 14:45:19 +0700

    ci: add github actions workflow for automatic github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // Giúp file build chạy được trên mọi đường dẫn (kể cả repo con của Github Pages)
 })
