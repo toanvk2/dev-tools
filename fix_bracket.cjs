fc032de24f35b57a2940a91fbddfb38f70fc47aa
toanvk9 <toanvk9@ghtk.co>
2026-09-10 15:35:23 +0700

    chore: remove temporary patch scripts

--- fix_bracket.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/App.tsx', 'utf8');
-code = code.replace("{ { key: '/html-viewer'", "{ key: '/html-viewer'");
-fs.writeFileSync('src/App.tsx', code);
--- patch_scroll.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/pages/YamlConverter.tsx', 'utf8');
-
-const oldOptions = "options={{ minimap: { enabled: false }, fontSize: 14, wordWrap: 'on' }}";
-const newOptions = "options={{ minimap: { enabled: false }, fontSize: 14, wordWrap: 'on', scrollBeyondLastLine: false, padding: { top: 16, bottom: 16 } }}";
-
-code = code.replaceAll(oldOptions, newOptions);
-
-fs.writeFileSync('src/pages/YamlConverter.tsx', code);
